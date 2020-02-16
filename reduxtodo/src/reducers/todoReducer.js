41265d30a3b780f41676272d14f02b548f6e599d
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-02-16 22:32:31 +0000

    initial state object

---
+++ reduxtodo/src/reducers/todoReducer.js
@@ -0,0 +1,14 @@
+
+
+// My initial state object:  a list of todos 
+
+export const initialState = 
+{
+   item: 'Learn about reducers',
+  completed: false,
+   id: 3892987589
+ }
+
+
+
+// ! Export  the reducer!

35586b2828379cdb43786515546b7f9e81de79c7
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-02-17 09:03:27 +0000

    added reducer hook and set up with a form

--- reduxtodo/src/App.js
+++ reduxtodo/src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
 import './App.css';
+import { initialState, toDoReducer } from "./reducers/todoReducer";
 
 function App() {
+
+  //Use our todoreducer hook here, need to import it 
+  //if we want our input field: [newTitleText] = useState(state.title) = keeps track of all updates
+
+  const [newTaskText, setNewTaskText] = useState('')
+  const [state, dispatch] = useReducer(toDoReducer, initialState);
+
+  const handleChanges = e => {
+    setNewTaskText(e.target.value)
+}
+
+
   return (
     <div className="App">
       
 
 
+      <input
+       type="text"
+       name="newTaskText"
+       value={newTaskText}
+       onChange={handleChanges}
+       />
+       <button> Add Task </button>
       
     </div>
   );
--- reduxtodo/src/reducers/todoReducer.js
+++ reduxtodo/src/reducers/todoReducer.js
@@ -5,10 +5,39 @@
 export const initialState = 
 {
    item: 'Learn about reducers',
-  completed: false,
+completed: false,
    id: 3892987589
  }
 
 
+ export const toDoReducer = (state, action) => {
+    switch(action.type) {
+        // case '' :
+        //     return {
 
-// ! Export  the reducer!
+        //     };
+
+            default:
+        
+        return state
+    };
+    
+
+ }
+
+//Going to have a case where we build a new array for our todos: find which one we clicked on and toggle its editing state
+//can do the same for filter() "FILTER_TODOS"
+//adding: use spread operator: todos: [...state.todo, newToDo] or could be action.payload 
+
+// case "TOGGLE_TODO"
+// return {
+   // todos: state.todos.map()
+
+
+// }
+
+// case "ADD_TODO"
+// const newToDo
+// return {
+   // todos: state.todos.map()
+// }
